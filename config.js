7c1d06feeb5d5016140e38d3583a20e2cfc4f5d3
Nguyen-Thuy10 <86759081+Nguyen-Thuy10@users.noreply.github.com>
2021-07-01 22:38:41 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Anh zai  à!',
+    introDesc: `Vậy thì nhiều tiền để làm gì?
+    Chả hiểu nhiều tiền để làm gì?
+    Nhiều tiền để mình, làm, được, thật là, nhiều điều thần kì
+    Yeah! Yeah!` ,
+    btnIntro: 'hihi',
+    title: 'Xung quanh anh toàn là nước cơ thể anh đang bị ướt',
+    desc: 'Mênh mông toàn là nước êm ái như chưa từng trước đây',
+    btnYes: 'xin 20k nạp tiền điện thoại ',
+    btnNo: 'Trong cơn mơ :3',
+    question:'tùy tâm nhé 😆😆 ',
+    btnReply: 'Gửi cho em 🤣 ',
+    reply: 'hehe',
+    mess: 'Em xin nhé 😁😁',
+    messDesc: 'Được khum bạn ơi 😆😆 ',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
